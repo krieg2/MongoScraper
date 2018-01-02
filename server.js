443bd69d3fe83957ace17276b6807c3d14b186a4
krieg2 <aaronk1@yahoo.com>
2018-01-02 17:55:07 -0500

    edit mongodb URI

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(morgan('combined'));
 
 // Database configuration
-mongoose.connect("mongodb://localhost/mongoHeadlines");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
